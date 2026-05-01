a4174dce4a527c452524b87093863fe896a1ee16
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-05-02 03:34:26 +0530

    added meals, add meals and haptics

--- macrozone/src/components/MacroGrid.tsx
+++ macrozone/src/components/MacroGrid.tsx
@@ -1,13 +1,46 @@
 import { StyleSheet, View } from "react-native";
 import MacroCard from "./MacroCard";
+import { Meal } from "@/storage/meals";
 
-export default function MacroGrid() {
+type MacroCardProps = {
+  meals: Meal[]
+}
+
+export default function MacroGrid({ meals }: MacroCardProps) {
+  const totals = meals.reduce((acc, meal) => ({
+    calories: acc.calories + meal.calories,
+    protein: acc.protein + meal.protein,
+    carbs: acc.carbs + meal.carbs,
+    fat: acc.fat + meal.fat
+  }),
+    { calories: 0, protein: 0, carbs: 0, fat: 0 }
+  )
   return (
     <View style={styles.grid}>
-      <MacroCard label="colories" value="0" goal="2000" color='#ff6b6b' />
-      <MacroCard label='Protein' value='0g' goal='150g' color='#4ecdc4' />
-      <MacroCard label='Carbs' value='0g' goal='250g' color='#ffd93d' />
-      <MacroCard label='Fat' value='0g' goal='65g' color='#6bcb77' />
+      <MacroCard
+        label='Calories'
+        value={`${totals.calories}`}
+        goal='2,000'
+        color='#ff6b6b'
+      />
+      <MacroCard
+        label='Protein'
+        value={`${totals.protein}g`}
+        goal='150g'
+        color='#4ecdc4'
+      />
+      <MacroCard
+        label='Carbs'
+        value={`${totals.carbs}g`}
+        goal='250g'
+        color='#ffd93d'
+      />
+      <MacroCard
+        label='Fat'
+        value={`${totals.fat}g`}
+        goal='65g'
+        color='#6bcb77'
+      />
     </View>
   )
 }
---
+++ macrozone/src/components/RecentMeals.tsx
@@ -0,0 +1,33 @@
+import { Text, View } from "react-native";
+import MealItem from "./MealItem";
+import { globalStyles } from "@/styles/global";
+import { Meal } from "@/storage/meals";
+
+type RecentMealsProps = {
+  meals: Meal[];
+  onDelete: () => void;
+}
+
+export default function RecentMeals({ meals, onDelete }: RecentMealsProps) {
+  return (
+    <View style={{ marginTop: 30 }}>
+      <Text style={globalStyles.sectionTitle}>Recent Meals</Text>
+      {meals.length === 0 ? (
+        <Text style={globalStyles.empty}>No meals logged yet.</Text>
+      ) : (
+        meals.slice(0, 5).map((meal) => (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            calories={meal.calories}
+            protein={meal.protein}
+            carbs={meal.carbs}
+            fat={meal.fat}
+            onDelete={onDelete}
+          />
+        ))
+      )}
+    </View>
+  )
+}
