a5b891a2e00715d53c0a441ed9ea8d23e17b88cb
shinas-rgb <shinasmuhammedkt@gmail.com>
2026-08-16 17:58:44 +0530

    create: added a screen to create place/post

---
+++ trekwiki/src/app/(tabs)/new-data.jsx
@@ -0,0 +1,23 @@
+import { useLocalSearchParams } from "expo-router"
+import AddPlaceScreen from "../components/add-place"
+import AddPostScreen from "../components/add-post"
+import { StyleSheet, View } from "react-native"
+import { colors } from "@/styles/global"
+
+export default function NewDataScreen() {
+  const {type} = useLocalSearchParams()
+  console.log("type: " + type)
+  return (
+    <View style={styles.container}>
+    {type === "post" && <AddPostScreen />}
+    {type === "place" && <AddPlaceScreen />}
+</View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.zinc900,
+  },
+})
